05b90f8ddb4758ec5d920e25997c963e421d3996
Jaime Garcia <garciar.jaime@gmail.com>
2019-03-04 21:19:43 -0500

    Making updates to make the project initially compilable

---
+++ src/main/java/com/bridgephase/store/Inventory.java
@@ -0,0 +1,17 @@
+package com.bridgephase.store;
+
+import java.io.InputStream;
+import java.util.List;
+
+import com.bridgephase.store.interfaces.IInventory;
+
+public class Inventory implements IInventory {
+    @Override
+    public void replenish(InputStream inputStream) {
+    }
+    
+    @Override
+    public List<Product> list() {
+        return null;
+    }
+}
